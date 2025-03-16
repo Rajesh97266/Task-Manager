500b92d6e98ad13addb3f96da5e8aec0132d5554
Rajesh97266 <rajesh97266@gmail.com>
2025-03-16 07:25:41 +0530

    react-router-setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,38 @@
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "sonner";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
+import Trash from "./pages/Trash";
+import TaskDetails from "./pages/TaskDetails";
+function Layout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
-  
-
   return (
-    <>
-      <Login />
-    </>
+    <main className="w-full min-h-screen bg-[#f3f4f6] ">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+        <Route path="/log-in" element={<Login />} />
+      </Routes>
+      <Toaster richColors />
+    </main>
   );
 }
 
