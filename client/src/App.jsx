0b1fd1a1e43428ed1908553786091376caf6f651
Rajesh97266 <rajesh97266@gmail.com>
2025-03-17 18:11:24 +0530

    login-left-side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
 import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
-import Tasks from "./pages/Tasks";
-import Users from "./pages/Users";
-import Trash from "./pages/Trash";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import TaskDetails from "./pages/TaskDetails";
+import Tasks from "./pages/Tasks";
+import Trash from "./pages/Trash";
+import Users from "./pages/Users";
+
 function Layout() {
-  return (
-    <div>
-      <Outlet />
+  const user = "";
+  const location = useLocation();
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+     <div className="w-1/5 h-screen bg-slate-300 sticky top-0 hidden md:block">
+      {/* <Sidebar /> */}
+     </div>
+     {/* <MobileSidebar /> */}
+     <div className="flex-1 overflow-y-auto">
+      {/* <Navbar /> */}
+      <div className="p-4 2xl:p-10">
+        <Outlet /> 
+      </div>
+     </div>
     </div>
+  ) : (
+    <Navigate to="/log-in" state={{ from: location }} replace />
   );
 }
 
@@ -19,7 +34,7 @@ function App() {
     <main className="w-full min-h-screen bg-[#f3f4f6] ">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/completed/:status" element={<Tasks />} />
